229d0bcadafab8a9d9c7782dcebdd5739c2742da
nikita <nikita1100@bk.ru>
2022-08-04 15:59:19 +0300

    Add second ms

---
+++ dps_register/src/main/java/com/example/dps_register/DpsRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.example.dps_register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpsRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DpsRegisterApplication.class, args);
+    }
+
+}
---
+++ insurance_register/src/test/java/com/example/insurance_register/InsuranceRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.insurance_register;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceRegisterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
